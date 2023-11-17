8bcbcdefe9e97daf541cde22798dde9b2ec681fa
mdrabiulis <mdrabiulis155@gmail.com>
2023-11-18 01:28:54 +0600

    vercel.json add

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
+
+// console.log(process.env.ACCESS_TOKEN_SECRET);
+
+const verifyCookies = async (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    //error
+    if (error) {
+      console.log(error);
+      return res.status(401).send({ message: "unauthorized" });
+    }
+    req.user = decoded;
+    console.log(decoded);
+    next();
+  });
+  // next();
+};
 
 const uri = `mongodb+srv://${process.env.NAME}:${process.env.NAME_KEY}@cluster0.wvvqbv9.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,25 +53,44 @@ async function run() {
     const allAddjob = client.db("Addjob").collection("job");
     const bidjobs = client.db("Addjob").collection("bidjob");
 
-    app.post("/bidjobs", async (req, res) => {
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        // expiresIn: "2s",
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
+
+    app.post("/bidjobs",  async (req, res) => {
       const AddBidjobData = req.body;
       const result = await bidjobs.insertOne(AddBidjobData);
       res.send(result);
-      // console.log(AddjobData);
     });
 
-    app.get("/bidjobs/:id", async (req, res) => {
+    app.get("/bidjobs/:id",  async (req, res) => {
       const id = req.params.id;
       const find = { _id: new ObjectId(id) };
       const result = await bidjobs.findOne(find);
       res.send(result);
     });
 
-    app.patch("/bidjobs/:id", async (req, res) => {
+    app.patch("/bidjobs/:id",  async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedBooking = req.body;
-      console.log(updatedBooking);
       const updateDoc = {
         $set: {
           status: updatedBooking.status,
@@ -51,7 +100,8 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/bidjobs", async (req, res) => {
+    // user bids find with email
+    app.get("/bidjobs", verifyCookies, async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -66,10 +116,9 @@ async function run() {
       const AddjobData = req.body;
       const result = await allAddjob.insertOne(AddjobData);
       res.send(result);
-      // console.log(AddjobData);
     });
 
-    app.put("/alljob/:id", async (req, res) => {
+    app.put("/alljob/:id",  async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
@@ -89,33 +138,35 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/alljob/:id", async (req, res) => {
+    app.get("/alljob/:id",  async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await allAddjob.findOne(query);
       res.send(result);
-      // console.log(result);
     });
 
     app.get("/alljob", async (req, res) => {
       const cursor = allAddjob.find();
       const result = await cursor.toArray();
       res.send(result);
-      // console.log(result);
     });
 
-    app.get("/alljobs", async (req, res) => {
-      console.log(req.query);
+    //Posted Jobs find with email
+    app.get("/alljobs", verifyCookies, async (req, res) => {
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       let query = {};
-      if (req.query?.email) {
+      if (req.query.email) {
         query = { email: req.query.email };
       }
       const result = await allAddjob.find(query).toArray();
       res.send(result);
     });
 
-    app.delete("/alljobs/:id", async (req, res) => {
+    app.delete("/alljobs/:id", verifyCookies, async (req, res) => {
       const id = req.params.id;
+
       const query = { _id: new ObjectId(id) };
       const result = await allAddjob.deleteOne(query);
       res.send(result);
